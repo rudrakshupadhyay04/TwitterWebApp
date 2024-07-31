b3cdb09c11d6430e8ea6b93ab15325c8f8ea45ea
rudrakshupadhyay04 <149106390+rudrakshupadhyay04@users.noreply.github.com>
2024-07-31 23:27:30 +0530

    Complete login page ui

--- FrontEnd/twitterclone/src/pages/Login.js
+++ FrontEnd/twitterclone/src/pages/Login.js
@@ -1,9 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { RiTwitterXLine } from 'react-icons/ri'
 
 const Login = () => {
+  const [isLogin, setIsLogin] = useState(true);
+
+  const loginSignupHandler = () => {
+    setIsLogin(!isLogin);
+  }
+
   return (
-    <div>Login</div>
+    <div className='w-screen h-screen flex items-center justify-center'>
+      <div className='flex items-center justify-evenly w-4/5'>
+
+        <div>
+          <RiTwitterXLine size={"300px"}/>
+        </div>
+
+        <div>
+          <div className='my-5'>
+            <h1 className='font-bold text-6xl'>Happening now.</h1>
+          </div>
+          <h1 className='mt-4 mb-2 text-2xl font-bold'>{isLogin ? "Login" : "Signup"}</h1>
+          <form className='flex flex-col w-3/5'>
+            {
+              !isLogin && (<>
+                <input type='text' placeholder='Username' className='outline-blue-500 border border-gray-800 px-3 py-2 rounded-full my-1 font-semibold'/>
+                <input type='text' placeholder='Name' className='outline-blue-500 border border-gray-800 px-3 py-2 rounded-full my-1 font-semibold'/>
+              </>)
+            }
+
+            <input type='text' placeholder='Email' className='outline-blue-500 border border-gray-800 px-3 py-2 rounded-full my-1 font-semibold'/>
+            <input type='text' placeholder='Password' className='outline-blue-500 border border-gray-800 px-3 py-2 rounded-full my-1 font-semibold'/>
+            <button className='bg-[#1D9BF0] border-none py-2 my-4 rounded-full text-lg text-white'>{isLogin ? "Login" : "Create Account"}</button>
+            <h1>{isLogin ? "Do not have an account?" : "Already have an account? "}<span onClick={loginSignupHandler} className='font-bold text-blue-600 cursor-pointer'>{isLogin ?  "Signup" : "Login"}</span></h1>
+          </form>
+        </div>
+      </div>
+    </div>
   )
 }
 
-export default Login
+export default Login 
