8b5fe6bfd4c42e2c674c1df630dc8c00ea0b4557
rudrakshupadhyay04 <149106390+rudrakshupadhyay04@users.noreply.github.com>
2024-08-06 19:45:26 +0530

    create authantication

---
+++ BackEnd/config/auth.js
@@ -0,0 +1,26 @@
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export const isAuthanticated = async (req , res, next){
+    try {
+        const token = req.cookies.token;
+        console.log(token);
+
+        if(!token){
+            return res.status(401).json({
+                message: "User is not authanticated!!!!",     
+                success: false       
+            })
+        }
+
+        const verify = jwt.verify(token , process.env.TOKEN_SECRET);
+        console.log(verify);
+        req.user = verify.id;
+        next();
+        
+    } catch (error) {
+        console.log(error);
+    }
+}
