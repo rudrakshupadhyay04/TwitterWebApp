f9c4e0a65f6a4f17e36b376ec3236cc666cc3cb3
rudrakshupadhyay04 <149106390+rudrakshupadhyay04@users.noreply.github.com>
2024-08-08 17:30:23 +0530

    Liked & Disliked Tweet api

--- BackEnd/controllers/tweet.controller.js
+++ BackEnd/controllers/tweet.controller.js
@@ -1,4 +1,5 @@
 import { Tweet } from "../models/tweet.model.js";
+import { User } from "../models/user.model.js";
 
 export const createTweet = async (req, res) => {
     try {
@@ -41,4 +42,31 @@ export const deleteTweet = async (req , res ) => {
     } catch (error) {
         console.log(error);
     }
+};
+
+export const likedOrDisliked = async (req, res) => {
+    try {
+        const loggedInUserId = req.body.id;
+        const user =  await User.findById(loggedInUserId);
+
+        const tweetId = req.params.id;
+        const tweet = await Tweet.findById(tweetId);
+
+        if(tweet.like.includes(loggedInUserId)){
+            // dislike
+            await Tweet.findByIdAndUpdate(tweetId,{$pull:{like:loggedInUserId}});
+            res.status(200).json({
+                message:`${user.name} disliked your tweet`
+            })
+        }else{
+            // like
+            await Tweet.findByIdAndUpdate(tweetId,{$push:{like:loggedInUserId}});
+            res.status(200).json({
+            message:`${user.name} liked your tweet`
+            })
+        }
+    } catch (error) {
+        console.log(error)
+    }
+
 }
--- BackEnd/routes/tweet.routes.js
+++ BackEnd/routes/tweet.routes.js
@@ -1,10 +1,11 @@
 import expres from 'express';
 import { isAuthanticated } from '../config/auth.js';
-import { createTweet, deleteTweet } from '../controllers/tweet.controller.js';
+import { createTweet, deleteTweet, likedOrDisliked } from '../controllers/tweet.controller.js';
 
 const router = expres.Router();
 
 router.post("/create", isAuthanticated ,createTweet);
-router.delete("/delete/:id", deleteTweet);
+router.delete("/delete/:id", isAuthanticated,deleteTweet);
+router.put("/like/:id", isAuthanticated,likedOrDisliked);
 
 export default router;
