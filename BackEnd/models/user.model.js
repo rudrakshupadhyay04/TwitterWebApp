d75c3cd62533f32a52fc2b7210472f86fc1150f9
rudrakshupadhyay04 <149106390+rudrakshupadhyay04@users.noreply.github.com>
2024-08-09 23:45:46 +0530

    Update user & tweet model

--- BackEnd/models/user.model.js
+++ BackEnd/models/user.model.js
@@ -20,6 +20,10 @@ const userSchema = new mongoose.Schema(
             type: String,
             required: true,
         },
+        bookmarks: {
+            type: Array,
+            default: [],
+        },
         followers:{
             type: Array,
             default: []
